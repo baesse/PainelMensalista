b80bcc7313725afd43678947adbd4153aebc9c3f
Igor <igorbaesse86@gmail.com>
2017-06-14 12:15:19 -0300

    Adicionar arquivos de projeto.

---
+++ PainelApostilamento/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PainelApostilamento.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            List<Models.Protocolo> PROTOCOLOS = new List<Models.Protocolo>();
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "Concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "não concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "Concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "Concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "não concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "Concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "não concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "Concluido"));
+            PROTOCOLOS.Add(new Models.Protocolo("15151/2017", "Sergio Orlando", "15/06", "Concluido"));
+
+
+           
+
+            
+            return View("Index",PROTOCOLOS);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PainelApostilamento/Models/Protocolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PainelApostilamento.Models
+{
+    public class Protocolo
+    {
+        public String PROTOCOLO;
+        public String NomeCliente;
+        public String Revisao;
+        public String Pronto;
+
+
+        public Protocolo(String PROTOCOLO, String NomeCliente, String Revisao, String Pronto)
+        {
+            this.PROTOCOLO = PROTOCOLO;
+            this.NomeCliente = NomeCliente;
+            this.Revisao = Revisao;
+            this.Pronto = Pronto;
+
+        }
+
+    }
+}
